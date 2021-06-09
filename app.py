336ed862f73f35cb48fe90d90d3d2ff4344c9791
HyungJin_Bang <37135305+Banghyungjin@users.noreply.github.com>
2021-06-09 12:20:46 +0900

    2021-06-09_02
    게시판기능추가
    matplotlib로 랜덤한 점 생성후 그래프로그리는 기능 추가

--- app.py
+++ app.py
@@ -9,6 +9,14 @@ app = Flask(__name__)
 
 app.secret_key = 'my_secret_key'
 
+db = pymysql.connect(
+            host='localhost',
+            port=3306,
+            user='root',
+            password='1234',
+            db='flasktest'
+        )
+
 
 # 메인 화면& 로그인
 @app.route('/', methods=['GET', 'POST'])
@@ -16,13 +24,6 @@ def index():
     if session.get('is_logged') is not None:
         return render_template("index.html", user=session.get('is_logged'))
     elif request.method == "POST":
-        db = pymysql.connect(
-            host='localhost',
-            port=3306,
-            user='root',
-            password='1234',
-            db='flasktest'
-        )
         cursor = db.cursor()
         sql = 'SELECT PW FROM login where ID = %s;'
         usid = request.form['Username']
@@ -45,13 +46,6 @@ def index():
 @app.route('/register', methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        db = pymysql.connect(
-            host='localhost',
-            port=3306,
-            user='root',
-            password='1234',
-            db='flasktest'
-        )
         cursor = db.cursor()
         name = request.form['Name']
         email = request.form['Email']
@@ -74,6 +68,86 @@ def log_out():
     session.clear()
     return render_template("log_in.html")
 
+# db 목록 읽어오는 기능
+@app.route('/articles', methods=["GET", "POST"])
+def articles():
+    if session.get('is_logged') is not None:
+        cursor = db.cursor()
+        sql = 'SELECT * FROM topic'
+        cursor.execute(sql)
+        topics = cursor.fetchall()
+        # articles = Articles()
+        # print(articles[0]['title'])
+        return render_template("articles.html", articles=topics)
+    else:
+        return render_template("log_in.html")
+
+
+# articles 선택한 걸 자세히 표시하는 기능
+@app.route('/article/<int:id>')  # <id> 를 params 라고 해서 메소드에서 써먹을 수 있다.
+def article(id):
+    cursor = db.cursor()
+    # articles = Articles()
+    # article = articles[id - 1]
+    sql = 'SELECT * FROM topic WHERE id = {};'.format(id)
+    cursor.execute(sql)
+    topic = cursor.fetchone()
+    return render_template("article.html", article=topic)
+
+
+# 새로운 article 추가하는 기능
+@app.route('/add_articles', methods=["GET", "POST"])
+def add_articles():
+    cursor = db.cursor()
+    if request.method == "POST":
+        desc = request.form['Desc']
+        title = request.form['Title']
+        author = request.form['Author']
+        sql_insert = "INSERT INTO `flasktest`.`topic` (`title`, `body`, `author`) VALUES (%s, %s, %s);"
+        val = [title, desc, author]
+        cursor.execute(sql_insert, val)
+        db.commit()
+        topic = cursor.fetchall()
+        # db.close()
+        return redirect("/articles")
+    else:
+        return render_template("add_articles.html")
+
+
+# article을 제거하는 기능
+@app.route('/delete/<int:id>', methods=["POST"])
+def delete(id):
+    cursor = db.cursor()
+    sql_insert = "DELETE FROM topic WHERE id = {};".format(id)
+    cursor.execute(sql_insert)
+    db.commit()
+    topic = cursor.fetchall()
+    # db.close()
+    return redirect("/articles")
+
+
+# article을 수정하는 기능
+@app.route('/change_articles/<int:id>', methods=["GET", "POST"])
+def change_articles(id):
+    cursor = db.cursor()
+    if request.method == "POST":
+        desc = request.form['Desc']
+        title = request.form['Title']
+        author = request.form['Author']
+        sql_change = "UPDATE topic SET title = %s, body = %s, author = %s, create_date = NOW() WHERE (id = %s);"
+        val = [title, desc, author, id]
+        cursor.execute(sql_change, val)
+        db.commit()
+        topic = cursor.fetchall()
+        return redirect("/articles")
+    else:
+        cursor = db.cursor()
+    sql = 'SELECT * FROM topic WHERE id = {};'.format(id)
+    cursor.execute(sql)
+    topic = cursor.fetchone()
+    return render_template("change_articles.html", article=topic)
+
+
 # 정보화면 - 아직 만드는 중
 @app.route('/about')
 def about():
@@ -82,6 +156,32 @@ def about():
     else:
         return render_template("log_in.html")
 
+
+@app.route('/fig/<int:mean>_<int:var>')
+def fig(mean, var):
+    if session.get('is_logged') is not None:
+        plt.figure(figsize = (4,3))
+        xs = np.random.normal(mean, var, 100)
+        ys = np.random.normal(mean, var, 100)
+        plt.scatter(xs,ys, s= 100, marker = 'o', color ='red', alpha = 0.3)
+        img = BytesIO()
+        plt.savefig(img, format = 'png', dpi = 200)
+        img.seek(0)
+        # print(xs)
+        return send_file(img, mimetype = 'image/png')
+    else:
+        return render_template("log_in.html")
+
+
+@app.route('/graphes/')
+def graphes():
+    if session.get('is_logged') is not None:
+        # m, v = m_v.split('_')
+        # m, v = int(m), int(v)
+        m, v = 3, 5
+        return render_template("graphes.html", mean = m, var = v, width = 1000, height = 1000)
+    else:
+        return render_template("log_in.html")
 
 
 if __name__ == '__main__':
